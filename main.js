a71f5be32f7984af9c3ac2c0ef2e2fabcc25adfb
myckel1900 <dtdcrz1900@gmail.com>
2023-03-21 12:27:29 -0500

    version 4.0

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const btnToggle = document.querySelector('.toggle-btn');
 
-btnToggle.addEventListener('click',function(){ //vamos a agregar un evento de escucha a este boton cuando pase un click
-    document.getElementById('sidebar').classList.toggle('active'); //sidebar:cada vez que le de click traera el id llamado sidebar  classList.toggle('active') hace que se añada un clase si no la tiene y quitarla si es que ya la tiene
+btnToggle.addEventListener('click',function(){ 
+    document.getElementById('sidebar').classList.toggle('active'); 
 })
